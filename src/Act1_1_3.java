87b4e86c0e6f2edfc41e33e2a01b1bd528bfde5b
Jordi Ribellas <ribellas2002@gmail.com>
2022-09-21 22:46:58 +0200

    Activitat 1.1.3 Feta

--- src/Act1_1_3.java
+++ src/Act1_1_3.java
@@ -1,44 +1,46 @@
 import java.io.File;
-
 public class Act1_1_3 {
 
 	public static void main(String[] args) {
 		
-		try {
-			
-			File ruta = new File("C:\\Users\\Jordi\\Desktop\\DAM\\M6\\UF1\\Act1.1\\src\\Act1_1_3.java");
-			
-			String nomFitxer = ruta.getName();
-			
-			System.out.println(nomFitxer);
-			
-			String rutaPath = ruta.getPath();
-			
-			System.out.println(rutaPath);
-			
-			String rutaAbsoluta = ruta.getAbsolutePath();
-			
-			System.out.println(rutaAbsoluta);
-			
-			if(ruta.canWrite()) {
-				System.out.println("Es pot escriure");
-			}else {
-				System.out.println("No es pot escriure");
-			}
-			
-			if(ruta.canRead()) {
-				System.out.println("Es pot llegit");
-			}else {
-				System.out.println("No es pot llegir");
-			}
-			
-			
-			
-			
-		}catch (Exception e){
-			System.err.println("No s'ha trobat l'arxiu.  " + e.getMessage());
+		File ruta = new File("C:\\Users\\Jordi\\Desktop\\DAM\\M6\\UF1\\Act1.1\\src\\Act1_1_3.java");
+		
+		String nomFitxer = ruta.getName();
+		
+		System.out.println(nomFitxer);
+		
+		String rutaPath = ruta.getPath();
+		
+		System.out.println(rutaPath);
+		
+		String rutaAbsoluta = ruta.getAbsolutePath();
+		
+		System.out.println(rutaAbsoluta);
+		
+		if(ruta.canWrite()) {
+			System.out.println("Es pot escriure");
+		}else {
+			System.out.println("No es pot escriure");
 		}
 		
+		if(ruta.canRead()) {
+			System.out.println("Es pot llegit");
+			
+		}else {
+			System.out.println("No es pot llegir");
+			
+		}
+		
+		long bytes = ruta.length();
+		
+		System.out.println(String.format("%,d kilobytes", bytes));
+		
+		if(ruta.isDirectory()) {
+			System.out.println("Es un directori");
+			
+		}else if(ruta.isFile()) {
+			System.out.println("Es un fitxer");
+			
+		}	
 	}
-
 }
